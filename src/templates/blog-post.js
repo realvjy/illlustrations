7cee97ba5fc233eed880fad5ca82e668c5311a82
realvjy <vijayverma@Vijay-Ka-Mac.local>
2019-11-10 18:16:50 +0530

    fix render issue

---
+++ src/templates/blog-post.js
@@ -0,0 +1,97 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <article>
+          <header>
+            <h1
+            >
+              {post.frontmatter.title}
+            </h1>
+            <p
+              style={{
+                display: `block`,
+              }}
+            >
+              {post.frontmatter.date}
+            </p>
+            <p>{post.frontmatter.author}
+            {post.frontmatter.category}</p>
+            <img src={post.frontmatter.image.childImageSharp.fluid.src} />
+          </header>
+          <section dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr
+          />
+          <footer>
+          </footer>
+        </article>
+
+        <nav>
+          <ul
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date
+        author
+        category
+        tags
+        png{
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
