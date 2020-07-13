4134437307070b593f4900cf727b8b7677b2f680
realvjy@gmail.com <zomato@Zomatos-MacBook-Pro.local>
2020-07-13 23:29:50 +0530

    added designletter ink

--- src/components/hero.js
+++ src/components/hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className="hero_details">
             <div className="texts">
               <h1>Open source illustrations kit</h1>
-              <p>Designed <strong>100 awesome illustrations</strong> during 100 days of illustration challenge (Now added more than <strong>120+ </strong>illustrations). Now, you can download all illustrations completely <strong>free</strong> and <strong>use</strong> these to design awesome - landing pages, mobile app or presentations.</p>
+              <p>Designed <strong>100 awesome illustrations</strong> during 100 days of illustration challenge (Now added more than <strong>120+ </strong>illustrations). You can download all illustrations completely <strong>free</strong> and <strong>use</strong> these to design awesome - landing pages, mobile app or presentations.</p>
             </div>
 
             <div className="btn-lnks">
